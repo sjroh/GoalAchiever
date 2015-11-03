49a2376f08a8238e10245739767673c8ff27c012
sjroh <sijine@gmail.com>
2015-11-02 19:51:23 -0600

    Timer part done Need to working on add coin, RouletteActivity, CollectionActivity

--- GoalAchiever/app/src/main/java/roh/sijine/goalachiever/ResultActivity.java
+++ GoalAchiever/app/src/main/java/roh/sijine/goalachiever/ResultActivity.java
@@ -1,12 +1,8 @@
 package roh.sijine.goalachiever;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 /**
@@ -25,7 +21,8 @@ public class ResultActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_result);
 
-        message = savedInstanceState.getString("RESULT");
+        Intent intent = getIntent();
+        message = intent.getStringExtra("RESULT");
 
         mContentView = (TextView) findViewById(R.id.fullscreen_content);
         mContentView.setText(message);
--- GoalAchiever/app/src/main/java/roh/sijine/goalachiever/WatcherActivity.java
+++ GoalAchiever/app/src/main/java/roh/sijine/goalachiever/WatcherActivity.java
@@ -1,26 +1,19 @@
 package roh.sijine.goalachiever;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import info.hoang8f.widget.FButton;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningTaskInfo;
 
-import java.util.Calendar;
-import java.util.concurrent.TimeUnit;
+import info.hoang8f.widget.FButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -28,18 +21,19 @@ import java.util.concurrent.TimeUnit;
  */
 public class WatcherActivity extends AppCompatActivity {
 
+    final private int timeInterval = 60000;
     private WatcherActivity.UnlockedReceiver unlockedReceiver;
     private TextView fullscreenTextview;
     private int unlockCounter;
     private Context mContext;
     private FButton btnStop;
-
     private int hourPicked;
     private int minPicked;
     private int milliPicked;
-    private Calendar future;
-
-
+    private int totalMin;
+    private int timeLeft;
+    private CountDownTimer cdt;
+//    private Calendar future;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +44,35 @@ public class WatcherActivity extends AppCompatActivity {
         Intent intent = getIntent();
         hourPicked = intent.getIntExtra("hourPicked", 0);
         minPicked = intent.getIntExtra("minPicked", 0);
+        totalMin = hourPicked * 60 + minPicked;
+        timeLeft = totalMin;
         milliPicked = intent.getIntExtra("milliPicked", 0);
 
-
         mContext = this.getApplicationContext();
+
+        cdt = new CountDownTimer(milliPicked, 1) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+//                if (timeLeft != totalMin){
+//                    timeLeft -= 1;
+//                }
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(mContext, ResultActivity.class);
+                intent.putExtra("RESULT", "FROM TIMER");
+                startActivity(intent);
+                finish();
+            }
+        }.start();
 
         btnStop = (FButton) findViewById(R.id.btn_stop);
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                cdt.cancel();
                 Intent intent = new Intent(mContext, ResultActivity.class);
                 intent.putExtra("RESULT", "FROM STOP");
                 startActivity(intent);
@@ -71,6 +85,7 @@ public class WatcherActivity extends AppCompatActivity {
         unlockCounter = 0;
 
         registerReceiver(unlockedReceiver, new IntentFilter("android.intent.action.USER_PRESENT"));
+
 
     }
 
@@ -90,19 +105,8 @@ public class WatcherActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private class UnlockedReceiver extends BroadcastReceiver {
-        // The reason that I put this BroadcastReceiver in this activity is
-        // I couldn't find the way to call updateCounter function or capture onReceive
-        // from outside of the class.
-        public void onReceive(Context ctx, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)) {
-                updateCounter();
-            }
-        }
-    }
-
     public void updateCounter() {
-        unlockCounter ++;
+        unlockCounter++;
         Log.d("[WATCH_ACTIVITY]", "### count = " + unlockCounter);
 
         // for test purpose
@@ -112,6 +116,17 @@ public class WatcherActivity extends AppCompatActivity {
 
         // else keep going
 
+    }
+
+    private class UnlockedReceiver extends BroadcastReceiver {
+        // The reason that I put this BroadcastReceiver in this activity is
+        // I couldn't find the way to call updateCounter function or capture onReceive
+        // from outside of the class.
+        public void onReceive(Context ctx, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)) {
+                updateCounter();
+            }
+        }
     }
 }
 
