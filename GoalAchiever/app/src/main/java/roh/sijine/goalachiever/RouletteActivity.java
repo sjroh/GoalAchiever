57016aacfef23709385e4f8fbc3d43405a8956ff
ydy1128 <ydy1128@gmail.com>
2015-11-11 18:46:58 -0600

    roulette spin updated

--- GoalAchiever/app/src/main/java/roh/sijine/goalachiever/RouletteActivity.java
+++ GoalAchiever/app/src/main/java/roh/sijine/goalachiever/RouletteActivity.java
@@ -1,16 +1,30 @@
 package roh.sijine.goalachiever;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.AbsoluteLayout;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import info.hoang8f.widget.FButton;
 
 public class RouletteActivity extends AppCompatActivity {
 
     private Context mContext;
     private ScoreHandler sh;
     private TextView textCoin;
-
+    private ImageView roulette;
+    private FButton btnSpin;
+    private static final float rotate_from = 0.0f;
+    private static final float rotate_to = -10.0f * 360.0f;
     /*  README
     *       READ ScoreHandler
     *       You can add coins by using sh.addCoin(int)
@@ -31,9 +45,20 @@ public class RouletteActivity extends AppCompatActivity {
         textCoin = (TextView) findViewById(R.id.text_coin);
         textCoin.setText(String.valueOf(sh.getCoin()));
 
-        // TODO
-        // WRITE YOUR CODE
+        roulette = (ImageView) findViewById(R.id.roulette);
+        roulette.setImageResource(R.drawable.roulette);
 
+        btnSpin = (FButton) findViewById(R.id.btn_spin);
+        btnSpin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RotateAnimation r;
+                r = new RotateAnimation(rotate_from, rotate_to, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                r.setDuration((long) 2*1500);
+                r.setRepeatCount(0);
+                roulette.startAnimation(r);
+            }
+        });
     }
 
 }
